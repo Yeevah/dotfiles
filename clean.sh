1a05e49db220753f7075312ab658613dd83941dc
Prima Bagus Saputra <Yeevah@Silverbox>
2021-07-27 15:15:50 +0700

    add install and clean script

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make --directory=dwm/ clean
+make --directory=st/ clean
+make --directory=slstatus/ clean
+make --directory=slock/ clean
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make --directory=dwm/ install
+make --directory=st/ install
+make --directory=slstatus/ install
+make --directory=slock/ install
